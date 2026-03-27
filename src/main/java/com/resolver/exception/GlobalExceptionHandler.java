be6d6d6900551102cd835c159eb2da8ef9ca1981
shubhamdwi95-hue <shubhamdwi95@gmail.com>
2026-03-28 02:12:23 +0530

    Added global exception

---
+++ src/main/java/com/resolver/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.resolver.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+@ExceptionHandler(MethodArgumentNotValidException.class)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public String handleValidationException(MethodArgumentNotValidException ex){
+
+    return ex.getBindingResult()
+    .getFieldError()
+    .getDefaultMessage();
+
+}
+
+
+}
