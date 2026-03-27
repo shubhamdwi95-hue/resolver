2a4ecc8378771c2cfacc0b870b35eb0afa005e55
shubhamdwi95-hue <shubhamdwi95@gmail.com>
2026-03-28 02:31:53 +0530

    implemeted logger

--- src/main/java/com/resolver/service/TicketService.java
+++ src/main/java/com/resolver/service/TicketService.java
@@ -1,5 +1,7 @@
 package com.resolver.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -9,18 +11,29 @@ import com.resolver.repository.TicketRepository;
 
 @Service
 public class TicketService {
-    
+
+private static final Logger log = LoggerFactory.getLogger(TicketService.class);
+
 private final TicketRepository ticketRepository;
 
     public TicketService(TicketRepository ticketRepository) {
         this.ticketRepository = ticketRepository;
     }
-
     public Ticket createTicket(Ticket ticket) {
-        return ticketRepository.save(ticket);
+
+        log.info("Creating new ticket with title: {}", ticket.getTitle());
+
+        Ticket savedTicket = ticketRepository.save(ticket);
+
+        log.info("Ticket created with ID: {}", savedTicket.getId());
+
+        return savedTicket;
     }
 
     public List<Ticket> getAllTickets() {
+
+        log.info("Fetching all tickets");
+
         return ticketRepository.findAll();
     }
 }
